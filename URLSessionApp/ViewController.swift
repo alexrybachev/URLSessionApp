5264d9e7692d662a6599e8606fe932efa9f93874
Alex Rybachev <rybachev.as@gmail.com>
2022-04-08 00:05:25 +0400

    Initial Commit

---
+++ URLSessionApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  URLSessionApp
+//
+//  Created by Aleksandr Rybachev on 08.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
